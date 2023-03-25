977d49e7f2a9a9b94d0f34ca0684259b9196d39c
swapnil1305 <swapnil.satpute132@gmail.com>
2023-03-25 12:11:02 +0530

    ..

--- routes/chat.js
+++ routes/chat.js
@@ -7,6 +7,6 @@ const router = express.Router();
 router.get('/users/chat', chatcontroller.getchat);
 router.post('/users/chat', auntheticateController.authenticate, chatcontroller.postchat);
 
-router.post('/sendfile/:groupId',userAuthentication.authenticate,messageControllers.uploadFile);
+router.post('/sendfile/:groupId',auntheticateController.authenticate,messageControllers.uploadFile);
 
 module.exports = router;
